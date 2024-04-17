90e23271e7a4f21bc5e70ad3e0fe69830a95df30
TheSylvieChan <pieter.vanderlinde@protonmail.com>
2024-04-17 14:36:58 +0200

    added detection for large displays and popup window

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,17 @@ function fnMenu() {
     imgText.innerHTML = imgs.alt;
     expandImg.parentElement.style.display = "block";
   } 
+
+  function fnScreenSize(){
+    var w = document.documentElement.clientWidth || window.innerWidth;
+    var modal = document.getElementById("myModal");
+    if (w > 450 ) {
+        modal.style.display = "block";
+    }
+    var span = document.getElementsByClassName("close")[0];
+ 
+ span.onclick = function() {
+    modal.style.display = "none";
+  }
+  return 0;
+}
